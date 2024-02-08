89d28d45369749749325ea650818e07086413dbb
janpatrickpalacio <janpatrick.dev@gmail.com>
2024-02-08 11:18:21 +0800

    implemented upvotes and downvotes for Post and Comment

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,8 +1,8 @@
 module Api
   class CommentsController < ApiController
     before_action :set_post, except: %i[destroy]
-    before_action :set_comment, only: %i[update destroy]
-    before_action :authenticate_user!, only: %i[create update destroy]
+    before_action :set_comment, only: %i[update destroy upvote_comment downvote_comment]
+    before_action :authenticate_user!, only: %i[create update destroy upvote_comment downvote_comment]
 
     def index
       render json: ::CommentSerializer.new(@post.comments)
@@ -25,7 +25,7 @@ module Api
 
       if current_user && current_user.id == @comment.user.id
         @comment.update(comment_params)
-        render json: { message: 'Comment was updated successfully.' }
+        render json: ::CommentSerializer.new(@comment)
       else
         render json: { status: 401, message: 'Unauthorized' }
       end
@@ -38,6 +38,24 @@ module Api
       else
         render json: { status: 401, message: 'Unauthorized' }
       end
+    end
+
+    # CUSTOM ROUTES
+
+    def upvote_comment
+      toggle_vote('upvotes')
+      remove_vote('downvotes')
+
+      @comment.save
+      render json: ::CommentSerializer.new(@comment)
+    end
+
+    def downvote_comment
+      toggle_vote('downvotes')
+      remove_vote('upvotes')
+
+      @comment.save
+      render json: ::CommentSerializer.new(@comment)
     end
 
     private
@@ -54,6 +72,20 @@ module Api
 
       def set_comment
         @comment = Comment.find(params[:id])
+      end
+
+      def toggle_vote(vote_type)
+        if @comment.votes[vote_type].include?(current_user.id)
+          @comment.votes[vote_type].delete(current_user.id)
+        else
+          @comment.votes[vote_type] << current_user.id
+        end
+      end
+
+      def remove_vote(vote_type)
+        if @comment.votes[vote_type].include?(current_user.id)
+          @comment.votes[vote_type].delete(current_user.id)
+        end
       end
   end
 end
--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,7 +1,7 @@
 module Api
   class PostsController < ApiController
-    before_action :authenticate_user!, only: %i[create update destroy]
-    before_action :set_post, only: %i[show update destroy]
+    before_action :authenticate_user!, only: %i[create update destroy upvote_post downvote_post]
+    before_action :set_post, only: %i[show update destroy upvote_post downvote_post]
     
     def index
       posts = Post.where(is_draft: false).order(created_at: :desc)
@@ -42,12 +42,29 @@ module Api
       end
     end
 
+    # CUSTOM ROUTES
+
+    def upvote_post
+      toggle_vote('upvotes')
+      remove_vote('downvotes')
+
+      @post.save
+      render json: ::PostSerializer.new(@post)
+    end
+
+    def downvote_post
+      toggle_vote('downvotes')
+      remove_vote('upvotes')
+
+      @post.save
+      render json: ::PostSerializer.new(@post)
+    end
+
     private
       def post_params
         params.require(:post).permit(
           :title, 
           :body, 
-          :votes, 
           :is_draft,
           :votes => [:upvotes => [], :downvotes => []],
           tags: []
@@ -56,6 +73,20 @@ module Api
 
       def set_post
         @post = Post.find(params[:id])
+      end
+
+      def toggle_vote(vote_type)
+        if @post.votes[vote_type].include?(current_user.id)
+          @post.votes[vote_type].delete(current_user.id)
+        else
+          @post.votes[vote_type] << current_user.id
+        end
+      end
+
+      def remove_vote(vote_type)
+        if @post.votes[vote_type].include?(current_user.id)
+          @post.votes[vote_type].delete(current_user.id)
+        end
       end
   end
 end
